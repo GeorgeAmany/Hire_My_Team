a0b19c9d7dcb785a901fafaa9482405ffa0bfbf7
GeorgeAmany <106698653+GeorgeAmany@users.noreply.github.com>
2022-12-24 21:50:20 +0200

    first commit

--- lib/screens/home/view.dart
+++ lib/screens/home/view.dart
@@ -1,18 +1,1 @@
-import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Playing With API'),
-      ),
-      body: Center(
-        child: ElevatedButton(onPressed: () {}, child: Text("post")),
-      ),
-    );
-  }
-}
