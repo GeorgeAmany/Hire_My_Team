903046eec9ef3cf82571dccaae7268d9572270a9
GeorgeAmany <106698653+GeorgeAmany@users.noreply.github.com>
2022-12-25 20:56:11 +0200

    second commit

---
+++ lib/screens/home/model.dart
@@ -0,0 +1,20 @@
+class JobDetails {
+  JobDetails({
+    required this.companies,
+    required this.jobs,
+  });
+  late final List<String> companies;
+  late final List<String> jobs;
+
+  JobDetails.fromJson(Map<String, dynamic> json){
+    companies = List.castFrom<dynamic, String>(json['companies']);
+    jobs = List.castFrom<dynamic, String>(json['jobs']);
+  }
+
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['companies'] = companies;
+    _data['jobs'] = jobs;
+    return _data;
+  }
+}
